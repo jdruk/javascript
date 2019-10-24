9433ab8448ee34e9d439ebd1b8565f01ec513b5d
josafams <jo@speediomail.com>
2019-10-24 09:51:22 -0300

    Basic js

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,23 @@
+console.log("Opa")
+{
+    {
+        console.log("OPa")
+    }
+
+}
+
+let b = 2
+var a = 7
+const c = 6
+console.log([c, b , a])
+
+console.log(typeof "josafa")
+console.log(typeof {})
+console.log(typeof [])
+console.log(typeof 1)
+console.log(typeof 1.2)
+
+console.log(Number("2.0"))
+console.log(Number.isInteger(2.0)) // true
+console.log(Number.isInteger(2.1)) // false
+console.log(2.222235353.toFixed(2))
